58fac5f8ca9c71ad7ee815e69c1556be7860ee17
zhuxuyang <imzhuxuyang@gmail.com>
2019-12-07 20:49:10 +0800

    完成

---
+++ content-script.js
@@ -0,0 +1,16 @@
+﻿(function() {
+  console.log('这是 simple-chrdome-plugin-demo 的content-script！');
+  var test = window.location.href;
+  url = getQueryString('url');
+  // alert(url);
+  window.location.href = url;
+})();
+
+function getQueryString(name) {
+  var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
+  var r = window.location.search.substr(1).match(reg);
+  if (r != null) {
+    return unescape(r[2]);
+  }
+  return null;
+}
